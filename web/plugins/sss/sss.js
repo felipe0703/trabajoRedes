beed3e313ee8f7b449d1e474206bcd3d923070f4
felipe0703 <felipe.aguilera.g@gmail.com>
2017-09-22 12:22:57 -0300

    ultima actualizacion

--- web/plugins/sss/sss.js
+++ web/plugins/sss/sss.js
@@ -11,8 +11,8 @@ $.fn.sss = function(options) {
 	var settings = $.extend({
 	slideShow : true,
 	startOn : 0,
-	speed : 3500,
-	transition : 400,
+	speed : 4500,
+	transition : 600,
 	arrows : true
 	}, options);
 
